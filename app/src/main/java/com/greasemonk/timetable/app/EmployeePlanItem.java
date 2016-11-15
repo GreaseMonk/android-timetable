e5196a74faa3f6c246f08adcb8a58c7777610775
W Geertsma <anacondah@gmail.com>
2016-11-15 11:16:01 +0100

    - Added sample plan item object with implementation - Added main view implementation, TimeTable

---
+++ app/src/main/java/com/greasemonk/timetable/app/EmployeePlanItem.java
@@ -0,0 +1,76 @@
+package com.greasemonk.timetable.app;
+
+import com.greasemonk.timetable.AbstractRowItem;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Random;
+
+/**
+ * Created by Wiebe Geertsma on 15-11-2016.
+ * E-mail: e.w.geertsma@gmail.com
+ * <p>
+ * EmployeePlanItem
+ * sample class which is a substitute for your class.
+ */
+public class EmployeePlanItem implements AbstractRowItem
+{
+	
+	private String employeeName, projectName;
+	private Date planStart, planEnd;
+	
+	public EmployeePlanItem() {}
+	
+	public EmployeePlanItem(String employeeName, String projectName, Date planStart, Date planEnd)
+	{
+		this.employeeName = employeeName;
+		this.projectName = projectName;
+		this.planStart = planStart;
+		this.planEnd = planEnd;
+	}
+	
+	public static EmployeePlanItem generateSample()
+	{
+		final String[] firstNameSamples = {"Kristeen", "Carran", "Lillie", "Marje", "Edith", "Steve", "Henry", "Kyle", "Terrence"};
+		final String[] lastNameSamples = {"Woodham", "Boatwright", "Lovel", "Dennel", "Wilkerson", "Irvin", "Aston", "Presley"};
+		final String[] projectNames = {"Roof Renovation", "Mall Construction", "Demolition old Hallway"};
+		
+		// Generate a date range between now and 30 days
+		Random rand = new Random();
+		Calendar calendar = Calendar.getInstance();
+		calendar.add(Calendar.DATE, -rand.nextInt(30));
+		Date planStart = calendar.getTime();
+		calendar.add(Calendar.DATE, rand.nextInt(30));
+		Date planEnd = calendar.getTime();
+		
+		return new EmployeePlanItem(firstNameSamples[rand.nextInt(firstNameSamples.length + 1)] +
+				lastNameSamples[rand.nextInt(lastNameSamples.length + 1)],
+				projectNames[rand.nextInt(projectNames.length + 1)],
+				planStart,
+				planEnd);
+	}
+	
+	@Override
+	public Date getPlanningStart()
+	{
+		return planStart;
+	}
+	
+	@Override
+	public Date getPlanningEnd()
+	{
+		return planEnd;
+	}
+	
+	@Override
+	public String getEmployeeName()
+	{
+		return employeeName;
+	}
+	
+	@Override
+	public String getProjectName()
+	{
+		return projectName;
+	}
+}
--- app/src/main/java/com/greasemonk/timetable/app/MainActivity.java
+++ app/src/main/java/com/greasemonk/timetable/app/MainActivity.java
@@ -2,14 +2,32 @@ package com.greasemonk.timetable.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.greasemonk.timetable.TimeTable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
 {
+	private static final int GENERATED_AMOUNT = 20;
+	private TimeTable timeTable;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		timeTable = (TimeTable) findViewById(R.id.time_table);
+		
+	}
+	
+	private static List<EmployeePlanItem> generateSamplePlanData()
+	{
+		List<EmployeePlanItem> planItems = new ArrayList<>();
+		for(int i = 0; i < GENERATED_AMOUNT; i++)
+			planItems.add(EmployeePlanItem.generateSample());
+		
+		return planItems;
 	}
 }
